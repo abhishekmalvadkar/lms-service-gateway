6293b816458e599b673fc093193d7de7321f73cf
Abhishek Malvadkar <abhi@dev.com>
2025-03-22 11:18:48 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/lms/gateway/LmsServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.lms.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalvadkar/lms/gateway/LmsServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.lms.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
