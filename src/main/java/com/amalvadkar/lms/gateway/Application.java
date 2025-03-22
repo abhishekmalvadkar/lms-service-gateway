0b2f83524b22a37cbf615ac4d2b9d5d425d8eeaa
Abhishek Malvadkar <abhi@dev.com>
2025-03-22 11:34:00 +0530

    GH-1 : Just refactoring

--- src/main/java/com/amalvadkar/lms/gateway/Application.java
+++ src/main/java/com/amalvadkar/lms/gateway/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LmsServiceGatewayApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LmsServiceGatewayApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
